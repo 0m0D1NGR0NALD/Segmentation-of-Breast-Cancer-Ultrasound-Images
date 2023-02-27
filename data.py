a331187731859ee2449da61c924f659377a7558c
OMODING RONALD <chidojawbreaker@gmail.com>
2023-02-27 14:47:21 +0300

    commit

--- data.py
+++ data.py
@@ -10,3 +10,9 @@ path = Path(root_dir)
 image_paths = list(path.glob('*/*.png'))
 
 images = [str(image_path) for image_path in image_paths if '_mask' not in str(image_path)]
+masks = [str(image_path) for image_path in image_paths if '_mask' in str(image_path)]
+labels = [os.path.split(os.path.split(name)[0])[1] for name in images]
+
+classes = list(set(labels))
+labels_dict = {label: i for i,label in enumerate(classes)}
+labels = [labels_dict[label_key] for label_key in labels]
