018751e102ee918f982c09f879a2c3879706e6e4
OMODING RONALD <chidojawbreaker@gmail.com>
2023-02-28 13:13:49 +0300

    commit

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset,DataLoader
+from torchvision import transforms
 
 from pathlib import Path
+from PIL import Image
 import os
 
 root_dir = 'Dataset/'
@@ -14,3 +16,14 @@ labels = [os.path.split(os.path.split(name)[0])[1] for name in images]
 classes = list(set(labels))
 labels_dict = {label: i for i,label in enumerate(classes)}
 labels = [labels_dict[label_key] for label_key in labels]
+
+class CustomDataset(Dataset):
+    def __init__(self,image_path,target_path):
+        self.image_path = image_path
+        self.target_path = target_path
+        self.transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225])])
+    
