3b4f1f85a0bdccd34a3d6c3ea252697482580bf3
OMODING RONALD <chidojawbreaker@gmail.com>
2023-03-01 16:17:55 +0300

    commit

--- train.py
+++ train.py
@@ -1,6 +1,14 @@
+import torch
+from torch.optim import Adam
+import torch.nn as nn
+
+
 from model import UNet
 from data import train_loader,val_loader
 
 model = UNet()
 
+lr = 0.0001
+
 criterion = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(),lr=lr)
