dc1d80e992bc87716df6297b73a8878e676a59f2
OMODING RONALD <chidojawbreaker@gmail.com>
2023-03-01 16:47:20 +0300

    commit

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ import torch
 from torch.optim import Adam
 import torch.nn as nn
 import torchmetrics
-from torchmetrics.functional import dice_score
 
 from model import UNet
 from data import train_loader,val_loader
@@ -15,8 +14,8 @@ lr = 0.0001
 # Set number of epochs
 epochs = 2
 # Define loss function
-loss = dice_score()
+loss = torchmetrics.Dice()
 # Define optimizer
 optimizer = Adam(model.parameters(),lr=lr)
 # Define metrics
-metrics = [torchmetrics.IoU(threshold=0.5)]
+metrics = torchmetrics.IoU()
