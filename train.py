03525ba5d630d1690b81164f36a94e356c3fa784
OMODING RONALD <chidojawbreaker@gmail.com>
2023-03-01 16:38:20 +0300

    commit

--- train.py
+++ train.py
@@ -1,14 +1,22 @@
 import torch
 from torch.optim import Adam
 import torch.nn as nn
-
+import torchmetrics
+from torchmetrics.functional import dice_score
 
 from model import UNet
 from data import train_loader,val_loader
 
+# Initialise UNet model
 model = UNet()
 
+# Set learning rate
 lr = 0.0001
-
-criterion = nn.CrossEntropyLoss()
+# Set number of epochs
+epochs = 2
+# Define loss function
+loss = dice_score()
+# Define optimizer
 optimizer = Adam(model.parameters(),lr=lr)
+# Define metrics
+metrics = [torchmetrics.IoU(threshold=0.5)]
